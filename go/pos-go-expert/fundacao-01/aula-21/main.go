1e3ceb24d76ce604db517d6b8cb204ef7306540e
Tarantini Pereira <taranttini@hotmail.com>
2024-01-01 20:26:17 -0300

    modulos

---
+++ go/pos-go-expert/fundacao-01/aula-21/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"pos-go-expert/fundacao-01/aula-21/matematic"
+)
+
+func main() {
+	m := matematic.Sum(1, 2)
+	fmt.Println(m)
+}
---
+++ go/pos-go-expert/fundacao-01/aula-21/matematic/matematic.go
@@ -0,0 +1,5 @@
+package matematic
+
+func Sum(value1 int, value2 int) int {
+	return value1 + value2
+}
