3220c628c27ad37cc2e1e07badb0f38ef49c971c
Tarantini Pereira <taranttini@hotmail.com>
2024-01-17 22:51:44 -0300

    atualizando aulas, fileserver,template,timeout

---
+++ go/pos-go-expert/fundacao-02-pacotes/aula-18-httpclient-timeout/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+	client := http.Client{Timeout: 1 * time.Microsecond}
+	response, err := client.Get("https://google.com")
+	if err != nil {
+		println("timeout ao acessar o site")
+		return
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+}
