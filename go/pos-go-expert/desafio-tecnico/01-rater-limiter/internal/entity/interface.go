c6217efdbf3998a3bac8bd2cfc3686a509d6adb6
Tarantini Pereira <taranttini@hotmail.com>
2024-06-25 00:25:43 -0300

    ajustando nome dos destinos

--- go/pos-go-expert/desafio-tecnico/01-rater-limiter/internal/entity/interface.go
+++
@@ -1,6 +0,0 @@
-package entity
-
-type RateLimiterRepositoryInterface interface {
-	Insert(keyType string, keyValue string, expiresTimeInSecond int) error
-	Count(keyType string, keyValue string) (int, error)
-}
