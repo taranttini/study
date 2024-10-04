3c1135fcb558a491d147164ccc53bb80a4d46aad
Tarantini Pereira <tau@MacBook-Air-de-Tarantini.local>
2024-10-03 23:43:27 -0300

    feat: java estudo - atividade 1

---
+++ java/dio/desafio_01/ContaBanco/ContaTerminal.java
@@ -0,0 +1,54 @@
+package study.java.dio.desafio_01.ContaBanco;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        // leitura dos dados via input
+        // criando o objeto scanner
+
+        String nomeCliente = "";// [Nome Cliente]";
+        String agencia = "";// "[Agência]";
+        Integer numero = 0;// "[Número]";
+        Float saldo = (float) 0.0; // [Saldo]
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+            System.out.println("Por favor, digite o nome do cliente !");
+            nomeCliente = scanner.next();
+        } catch (Exception e) {
+            System.out.println("Erro ao obter o nome do cliente!");
+            return;
+        }
+        try {
+            System.out.println("Por favor, digite o código da Agência !");
+            agencia = scanner.next();
+        } catch (Exception e) {
+            System.out.println("Erro ao obter o código da agência!");
+            return;
+        }
+        try {
+            System.out.println("Por favor, digite o número da Conta !");
+            numero = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Erro ao obter o número da Conta!");
+            return;
+        }
+        try {
+            System.out.println("Por favor, digite o valor do saldo !");
+            saldo = scanner.nextFloat();
+        } catch (Exception e) {
+            System.out.println("Erro ao obter o valor do saldo");
+            return;
+        }
+        String aviso = "Olá " + nomeCliente +
+                ", obrigado por criar uma conta em nosso banco" +
+                ", sua agência é " + agencia +
+                ", conta " + numero +
+                " e seu saldo " + saldo +
+                " já está disponível para saque.";
+
+        System.out.println(aviso);
+    }
+}
