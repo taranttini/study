f5f345d2c703984e88a4a0ab06d45cc559ca086f
Tarantini Pereira <taranttini@hotmail.com>
2025-11-17 01:18:45 -0300

    interagindo com inputs e eventos

--- kotlin/dio/2-eletric-car-app/EletricCarApp/app/src/main/java/com/example/eletriccarapp/MainActivity.kt
+++ kotlin/dio/2-eletric-car-app/EletricCarApp/app/src/main/java/com/example/eletriccarapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.eletriccarapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var price: EditText
+    lateinit var traveled: EditText
+    lateinit var tvResult: TextView
+    lateinit var btnCalculate: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupViews()
+        setupListerners()
+    }
+
+    fun setupViews() {
+        price = findViewById(R.id.et_input_price)
+        traveled = findViewById(R.id.et_input_km_traveled)
+        tvResult = findViewById(R.id.tv_result)
+        btnCalculate = findViewById(R.id.btn_calculate)
+    }
+
+    @SuppressLint("SetTextI.8.18n")
+    fun calcular() {
+        val price = price.text.toString().toFloat();
+        val km = traveled.text.toString().toFloat();
+        val result = price / km;
+
+        tvResult.text = "KM percorrido : $result"
+    }
+
+    fun setupListerners() {
+
+        btnCalculate.setOnClickListener {
+            calcular()
+        }
+
     }
 }
